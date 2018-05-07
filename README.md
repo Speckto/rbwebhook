9fa6772e1404c07df9d4bac05ea3a0021bec3abd
Neil.Potter <Potter.Neil@outlook.com>
2018-05-07 15:41:36 +0000

    Validate needed form data has been provided. Update readme

--- README.md
+++ README.md
@@ -8,14 +8,48 @@ calls to specific areas of a perforce depot.
 
 Where perforce is being used as the source control system, a shelved change
 will be unshelved, falling back to applying the diff fetched from reviewboard
-directly (the diff doesn't deal with perforce moved files very well).
+directly (the diff doesn't deal with perforce moved files very well). The
+latter allows use with git-p4.
 
 # Status
 - Scripts work
 - Pipelines work
 - Use reviewboard webhooks to trigger Jenkins jobs is proving to be problematic
+- This is intended for use in a secured private network, and should not be
+  exposed to the internet.
+
+# Architecture
+- Reviewboard server webhook invokes URL on a proxy service providing
+  information about the review (url, id, commit id)
+- Proxy service forwards request Jenkins server, starting a build of a
+  jenkins job with parameters provided by reviewboard server
+- "callback" pipeline job in Jenkins receives request and identifies if it is
+  related to the perforce server area of interest.
+  If it is, invokes a build job
+- 'build' job which attempts to retrieve the changes from the review and posts
+  back success or failure
+## Proxy
+Flask-based "proxy" to receive webhook calls from reviewboard and forward
+to jenkins with proper authentication. Reviewboard does not appear capable of
+providing this authentication on a properly secured Jenkins deployment.
+
+# Reviewboard server configuration
+Configure a web hook
+- URL:  http://<proxy url>:<proxy port>/post
+- Encoding: Form data
+- "Use custom payload" checked
+- Custom content:
+```
+job=MMA-main-reviewbot-callback-pipeline&REVIEW_URL={{review_request.absolute_url}}&review_id={{review_request.id}}&review_commit_id={{review_request.commit_id}}
+```
+
+# Proxy
+A python "Flask" based microservice application is provided.
+Deploy this using your favourite microservice server.
 
 # TODO
 - The interface between some scripts and the Jenkins pipeline is currently
   shell exit codes which is rather clunky
+- Flask application needs better packaging
+- Provide deployment scripts for flask application (use uWSGI?)
 
--- webhook-service/RBJenkinsHookService.py
+++ webhook-service/RBJenkinsHookService.py
@@ -12,45 +12,55 @@ def result():
     # REVIEW_URL       - URL of review in reviewbaord
     # review_id        - The id for the review
     # review_commit_id - The "commit id" for the review
-    job = request.form['job']
-    reviewUrl = request.form['REVIEW_URL']
-    reviewId = request.form['review_id']
-    reviewCommitId = request.form['review_commit_id']
-    app.logger.info('Received request\n\
-                     job=%s\n\
-                     url=%s\n\
-                     id=%s\n\
-                     cid=%s',
+    if ('job' in request.form and
+        'REVIEW_URL' in request.form and
+        'review_id' in request.form and
+        'review_commit_id' in request.form):
+
+        job = request.form['job']
+        reviewUrl = request.form['REVIEW_URL']
+        reviewId = request.form['review_id']
+        reviewCommitId = request.form['review_commit_id']
+        app.logger.info('Received request\n\
+                         job=%s\n\
+                         url=%s\n\
+                         id=%s\n\
+                         cid=%s',
+                        job,
+                        reviewUrl,
+                        reviewId,
+                        reviewCommitId)
+
+        try:
+            server = jenkins.Jenkins(
+                'http://localhost:8080',
+                username='Jenkins.ReviewBot',
+                password='')
+
+            if server.job_exists(job):
+                app.logger.info("job %s exists. Attempting trigger...", job)
+                queueItemNo = server.build_job(
                     job,
-                    reviewUrl,
-                    reviewId,
-                    reviewCommitId)
+                    {'REVIEW_URL': reviewUrl,
+                     'review_id': reviewId,
+                     'review_commit_id': reviewCommitId})
+                queueItem = server.get_queue_item(queueItemNo)
+            else:
+                app.logger.info("ERROR: job does not exist in jenkins")
+                return 'ERROR: Job does not exist in jenkins'
 
-    try:
-        server = jenkins.Jenkins('http://localhost:8080',
-                                 username='Jenkins.ReviewBot',
-                                 password='')
+        except (jenkins.JenkinsException,
+                jenkins.NotFoundException,
+                jenkins.EmptyResponseException,
+                jenkins.BadHTTPException,
+                jenkins.TimeoutException) as error:
+            app.logger.info('Error interacting with Jenkins - error was:\n',
+                            error)
+            return 'ERROR: Failed to trigger:' + error
 
-        if server.job_exists(job):
-            app.logger.info("job %s exists. Attempting trigger...", job)
-            queueItemNo = server.build_job(
-                job,
-                {'REVIEW_URL': reviewUrl,
-                 'review_id': reviewId,
-                 'review_commit_id': reviewCommitId})
-            queueItem = server.get_queue_item(queueItemNo)
         else:
-            app.logger.info("ERROR: job does not exist in jenkins")
-            return 'ERROR: Job does not exist in jenkins'
-
-    except (jenkins.JenkinsException,
-            jenkins.NotFoundException,
-            jenkins.EmptyResponseException,
-            jenkins.BadHTTPException,
-            jenkins.TimeoutException) as error:
-        app.logger.info('Error interacting with Jenkins - error was:\n', error)
-        return 'ERROR: Failed to trigger:' + error
-
+            app.logger.info('OK: Job triggered')
+            return 'OK: Job triggered'
     else:
-        app.logger.info('OK: Job triggered')
-        return 'OK: Job triggered'
+        app.logger.info('ERROR: Missing parameter')
+        return 'ERROR: Missing parameter'
