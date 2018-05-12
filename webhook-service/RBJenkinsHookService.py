7a3adf38e2eb541d1717d5b0eb1d9fa1f0e84110
Neil.Potter <Potter.Neil@outlook.com>
2018-05-12 14:49:24 +0000

    Migrate from Flask to bottle for simplicity

--- webhook-service/RBJenkinsHookService.py
+++ webhook-service/RBJenkinsHookService.py
@@ -1,44 +1,38 @@
 import jenkins
-from flask import Flask
-from flask import request
+from bottle import Bottle, route, run, post, request
 
-app = Flask(__name__)
+app = Bottle()
 
-
-@app.route('/post', methods=['POST'])
+@app.post('/post')
 def result():
     # Expect 3 parameters
     # job              - Name of the jenkins job to trigger
     # REVIEW_URL       - URL of review in reviewbaord
     # review_id        - The id for the review
     # review_commit_id - The "commit id" for the review
-    if ('job' in request.form and
-        'REVIEW_URL' in request.form and
-        'review_id' in request.form and
-        'review_commit_id' in request.form):
+    if ('job' in request.forms and
+        'REVIEW_URL' in request.forms and
+        'review_id' in request.forms and
+        'review_commit_id' in request.forms):
 
-        job = request.form['job']
-        reviewUrl = request.form['REVIEW_URL']
-        reviewId = request.form['review_id']
-        reviewCommitId = request.form['review_commit_id']
-        app.logger.info('Received request\n\
-                         job=%s\n\
-                         url=%s\n\
-                         id=%s\n\
-                         cid=%s',
-                        job,
-                        reviewUrl,
-                        reviewId,
-                        reviewCommitId)
+        job = request.forms['job']
+        reviewUrl = request.forms['REVIEW_URL']
+        reviewId = request.forms['review_id']
+        reviewCommitId = request.forms['review_commit_id']
+        print 'Received request\n'\
+                         'job=' + job + '\n'\
+                         'url=' + reviewUrl + '\n'\
+                         'id=' + reviewId + 's\n'\
+                         'cid=' + reviewCommitId
 
         try:
             server = jenkins.Jenkins(
                 'http://localhost:8080',
                 username='Jenkins.ReviewBot',
-                password='')
+                password='dc446ad47c6c79fe4814f21ad594b023')
 
             if server.job_exists(job):
-                app.logger.info("job %s exists. Attempting trigger...", job)
+                print "job ' + job + ' exists. Attempting trigger..."
                 queueItemNo = server.build_job(
                     job,
                     {'REVIEW_URL': reviewUrl,
@@ -46,7 +40,7 @@ def result():
                      'review_commit_id': reviewCommitId})
                 queueItem = server.get_queue_item(queueItemNo)
             else:
-                app.logger.info("ERROR: job does not exist in jenkins")
+                print 'ERROR: job does not exist in jenkins'
                 return 'ERROR: Job does not exist in jenkins'
 
         except (jenkins.JenkinsException,
@@ -54,13 +48,14 @@ def result():
                 jenkins.EmptyResponseException,
                 jenkins.BadHTTPException,
                 jenkins.TimeoutException) as error:
-            app.logger.info('Error interacting with Jenkins - error was:\n',
-                            error)
+            print 'Error interacting with Jenkins - error was:\n' + error
             return 'ERROR: Failed to trigger:' + error
 
         else:
-            app.logger.info('OK: Job triggered')
+            print 'OK: Job triggered'
             return 'OK: Job triggered'
     else:
-        app.logger.info('ERROR: Missing parameter')
+        print 'ERROR: Missing parameter'
         return 'ERROR: Missing parameter'
+
+app.run(host='localhost', port=5000, debug=True)
